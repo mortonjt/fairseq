8fa7eff90f5e980e8db6fcf49147eb7b7ab68e95
Jamie Morton <jamietmorton@gmail.com>
2020-02-09 21:51:03 -0500

    Update model.py

--- fairseq/models/roberta/model.py
+++ fairseq/models/roberta/model.py
@@ -355,13 +355,16 @@ class RobertaEncoder(FairseqDecoder):
         return x, extra
 
     def extract_features(self, src_tokens, return_all_hiddens=False, **unused):
-        inner_states, _ = self.sentence_encoder(
+        inner_states, _, attns = self.sentence_encoder(
             src_tokens,
             last_state_only=not return_all_hiddens,
         )
         features = inner_states[-1].transpose(0, 1)  # T x B x C -> B x T x C
-        return features, {'inner_states': inner_states if return_all_hiddens else None}
-
+        states = {'inner_states': inner_states,
+                  'attns': attns
+                  if return_all_hiddens else None}
+        return features, states
+    
     def output_layer(self, features, masked_tokens=None, **unused):
         return self.lm_head(features, masked_tokens)
 
